9ad7a0527944a57ba774aba0f6058d2812d683f6
Brendan <bgillies@ualberta.ca>
2017-09-20 21:39:04 -0600

    api post calls are working, see if we can get it to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do 
-  
   namespace :api do
     root 'temperatures#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
