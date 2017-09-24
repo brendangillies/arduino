7336cc516b3d26bd245e84e86cdade5fdd7c120f
Brendan <bgillies@ualberta.ca>
2017-09-23 21:15:23 -0600

    post requests are working, added a landing page to display the data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do 
+  
+  root 'landing#index'
+  get 'landing/index'
+
+
+  
   namespace :api do
     root 'temperatures#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
